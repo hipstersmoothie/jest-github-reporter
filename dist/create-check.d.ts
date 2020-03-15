f9e629a26074600165c87051c4594d918d71240e
ajiron <ajiron@liveperson.com>
2020-03-15 14:20:01 -0700

    removed dist

--- dist/create-check.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="jest" />
-declare const _default: (results: jest.AggregatedResult, config: any) => Promise<void>;
-export default _default;
-//# sourceMappingURL=create-check.d.ts.map
--- dist/groupSequences.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: (numbers: readonly number[]) => number[][];
-/**
- * @param {Number[]} numbers an array of sorted numbers
- * @returns {Array[]} an array of array of numbers grouped by sequences
- */
-export default _default;
-//# sourceMappingURL=groupSequences.d.ts.map
