e58ec1dddb2fb78d791e1c39c93664b6166fd201
Pavel.Giletich <Pavel.Giletich@coxautoinc.com>
2023-03-02 01:20:34 +0100

    upd

--- src/create-check.ts
+++ src/create-check.ts
@@ -5,7 +5,7 @@ import { TestResult, AggregatedResult } from '@jest/reporters';
 import path from 'path';
 import stripAnsi from 'strip-ansi';
 import createCheck, { Annotation } from 'create-check';
-import execa from 'execa';
+import { execa } from 'execa';
 
 const APP_ID = 38833;
 /**
@@ -95,7 +95,7 @@ async function createAnnotations(results: TestResult[]) {
   return annotations;
 }
 
-export default (results: AggregatedResult) =>
+export default async (results: AggregatedResult) =>
   createCheck({
     tool: 'Jest',
     name: 'Test',
